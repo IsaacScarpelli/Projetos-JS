426c65582e0a078faeb026bc6e838801026fe9bc
Isaac Scarpelli <140021795+IsaacScarpelli@users.noreply.github.com>
2024-06-05 21:13:43 -0300

    Create Atv2Part1.js

---
+++ Atv2Part1.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contador</title>
+</head>
+<body>
+    <h1>Contador de 1 a 15</h1>
+    
+
+    <script type="text/javascript">
+        for (let i = 0; i <= 15; i++) {
+ console.log(i);
+}
+    </script>
+</body>
+</html>
